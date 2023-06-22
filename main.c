db6d22739c6a132fd7c09fab038d74b238aad755
Jon Daniel <jondaniel879@gmail.com>
2023-06-22 17:31:02 +0200

    separate update_progress_bar

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "progress.h"
 #include <sys/stat.h>
+#include <errno.h>
 
 void* buf = NULL;
 FILE* ifp = NULL;
@@ -18,21 +19,30 @@ int main(int argc, char** argv)
 
 	const char*  src = argv[1];
 	const char*  dst = argc > 2 ? argv[2] : argv[1];
+	/* get size of filename */
 	const size_t len = fsize(src);
+	if(len == 0)
+	{
+		fprintf(stderr, "%40s: File is empty!\n", src);
+		exit(EXIT_FAILURE);
+	}
+
+	/* initialize buffer and r/w status */
 	size_t read = 0;
 	size_t written = 0;
 	void* buf = malloc(len);
 
+	/* set exit cleanup func */
 	atexit(cleanup);
 
 	/* read from src */
 	FILE* ifp = fopen(src, "rb");
-	if((read = fread_progress_bar(buf, src, len, 8192, ifp, 40, '=', ' ')) != len)
+	if((read = fread_progress_bar(buf, src, len, 8192, ifp, 40, '|', '-')) != len)
 		exit(EXIT_FAILURE);
 
 	FILE* ofp = fopen(dst, "wb");
 	/* write to dst */
-	if((written = fwrite_progress_bar(buf, dst, len, 8192, ofp, 40, '=', ' ')) != len)
+	if((written = fwrite_progress_bar(buf, dst, len, 8192, ofp, 40, '|', '-')) != len)
 		exit(EXIT_FAILURE);
 
 	exit((read != len || written != len) ? EXIT_SUCCESS : EXIT_FAILURE);
@@ -42,11 +52,12 @@ size_t fsize(const char* filename)
 {
 	/* get file size */
 	struct stat sb;
-	if(stat(filename, &sb) == -1 || sb.st_size == 0)
+	if(stat(filename, &sb) == -1)
 	{
-		perror("lstat");
+		fprintf(stderr, "%40s: %s\n", filename, strerror(errno));
 		return 0;
 	}
+
 	return sb.st_size;
 }
 
--- progress.h
+++ progress.h
@@ -4,8 +4,21 @@
 #include <string.h>
 #include <sys/param.h>
 
+
+void update_progress_bar(char* bar, size_t fill_last, size_t fill_now, unsigned char width, char filled, char empty);
 size_t fread_progress_bar(void* ptr, const char* filename, size_t size, const size_t block_size, FILE* stream, unsigned char width, char filled, char empty);
 size_t fwrite_progress_bar(void* ptr, const char* filename, size_t size, const size_t block_size, FILE* stream, unsigned char width, char filled, char empty);
+
+void update_progress_bar(char* bar, size_t fill_last, size_t fill_now, unsigned char width, char filled, char empty)
+{
+	if(fill_last == 0)
+	{
+		memset(bar, empty, width);
+		bar[width] = '\0';
+	}
+	memset(bar + fill_last, filled, fill_now - fill_last);
+	bar[width] = '\0';
+}
 
 size_t fread_progress_bar(void* ptr, const char* filename, size_t size, const size_t block_size, FILE* stream, unsigned char width, char filled, char empty)
 {
@@ -14,10 +27,7 @@ size_t fread_progress_bar(void* ptr, const char* filename, size_t size, const si
 	unsigned char rotor = 0;
 	unsigned char progress = 0;
 	size_t transfered = 0;
-	/* initialize empty progress bar */
 	char bar[width];
-	memset(bar, empty, width);
-	bar[width] = '\0';
 
 	for(size_t res = 0; transfered < size; rotor = (rotor + 1) % sizeof(rotor_char))
 	{
@@ -25,15 +35,14 @@ size_t fread_progress_bar(void* ptr, const char* filename, size_t size, const si
 		const size_t len = MIN(size - transfered, block_size);
 		if((res = fread((unsigned char*)ptr + transfered, 1, len, stream)) == 0)
 			break;
-
-		size_t fill_last = progress * width / 100;
+		
 		/* increment progress */
+		size_t fill_last = progress * width / 100;
 		transfered += res;
 		progress = (unsigned char)(transfered * 100 / size);
 		size_t fill_now = progress * width / 100;
-		memset(bar + fill_last, '=', fill_now - fill_last);
-		memset(bar + fill_now, ' ', width - fill_now);
-		bar[width] = '\0';
+
+		update_progress_bar(bar, fill_last, fill_now, width, filled, empty);
 
 		/* read status progress bar output */
 		printf("\r%40s: [%.*s%.03hhu%%%s] %13zu %c", filename, 
@@ -51,15 +60,12 @@ size_t fread_progress_bar(void* ptr, const char* filename, size_t size, const si
 
 size_t fwrite_progress_bar(void* ptr, const char* filename, size_t size, const size_t block_size, FILE* stream, unsigned char width, char filled, char empty)
 {
-	/* write status progress bar variables */
+	/* read status progress bar variables */
 	const char rotor_char[5] = "/-\\|";
 	unsigned char rotor = 0;
 	unsigned char progress = 0;
 	size_t transfered = 0;
-	/* initialize empty progress bar */
 	char bar[width];
-	memset(bar, empty, width);
-	bar[width] = '\0';
 
 	for(size_t res = 0; transfered < size; rotor = (rotor + 1) % sizeof(rotor_char))
 	{
@@ -67,21 +73,20 @@ size_t fwrite_progress_bar(void* ptr, const char* filename, size_t size, const s
 		const size_t len = MIN(size - transfered, block_size);
 		if((res = fwrite((unsigned char*)ptr + transfered, 1, len, stream)) == 0)
 			break;
-
-		size_t fill_last = progress * width / 100;
+		
 		/* increment progress */
+		size_t fill_last = progress * width / 100;
 		transfered += res;
 		progress = (unsigned char)(transfered * 100 / size);
 		size_t fill_now = progress * width / 100;
-		memset(bar + fill_last, '=', fill_now - fill_last);
-		memset(bar + fill_now, ' ', width - fill_now);
-		bar[width] = '\0';
+
+		update_progress_bar(bar, fill_last, fill_now, width, filled, empty);
 
 		/* read status progress bar output */
 		printf("\r%40s: [%.*s%.03hhu%%%s] %13zu %c", filename, 
-		                         (int)fill_now, bar, progress,
-		                           bar + fill_now, transfered,
-		           rotor_char[transfered == size ? 0 : rotor]);
+		                               (int)fill_now, bar, progress,
+		                                 bar + fill_now, transfered,
+		                 rotor_char[transfered == size ? 0 : rotor]);
 
 		if(res < len)
 			break;
@@ -90,4 +95,3 @@ size_t fwrite_progress_bar(void* ptr, const char* filename, size_t size, const s
 
 	return transfered;
 }
-
